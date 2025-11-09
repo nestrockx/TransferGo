2b13fc88af69e5bfd510820da7f17231a63c6be6
nestrockx <wegielek.art@gmail.com>
2025-11-09 10:16:32 +0100

    refactor(fxRatesConverter): Remove force HTTPS interceptor
    Removes the unused `forceHttpsInterceptor` from the `CurrencyConverterNetworkModule`.

--- feature/fxRatesConverter/src/main/java/com/wegielek/feature/fxRatesConverter/di/CurrencyConverterNetworkModule.kt
+++ feature/fxRatesConverter/src/main/java/com/wegielek/feature/fxRatesConverter/di/CurrencyConverterNetworkModule.kt
@@ -2,26 +2,11 @@ package com.wegielek.feature.fxRatesConverter.di
 
 import com.wegielek.feature.fxRatesConverter.data.network.NetworkObserver
 import com.wegielek.feature.fxRatesConverter.data.remote.ExchangeRateApi
-import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-
-val forceHttpsInterceptor =
-    Interceptor { chain ->
-        val request = chain.request()
-        val httpsUrl =
-            request.url
-                .newBuilder()
-                .scheme("https")
-                .host("my.transfergo.com")
-                .build()
-
-        val httpsRequest = request.newBuilder().url(httpsUrl).build()
-        chain.proceed(httpsRequest)
-    }
 
 val currencyConverterNetworkModule =
     module {
